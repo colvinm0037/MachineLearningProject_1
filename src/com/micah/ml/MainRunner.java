1e8a1251f93cdf6ad386e3b0c7cf04097dad1d3e
Micah <colvinm0037@my.uwstout.edu>
2017-02-01 20:19:00 -0600

    Running all 5 algorithms on Student Data Set.

--- src/com/micah/ml/MainRunner.java
+++ src/com/micah/ml/MainRunner.java
@@ -4,12 +4,21 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import weka.classifiers.AbstractClassifier;
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
+import weka.classifiers.functions.LibSVM;
+import weka.classifiers.functions.MultilayerPerceptron;
+import weka.classifiers.lazy.IBk;
+import weka.classifiers.meta.AdaBoostM1;
 import weka.classifiers.trees.J48;
 import weka.core.Instances;
+import weka.core.WekaPackageManager;
+import weka.core.converters.LibSVMLoader;
+import weka.core.converters.LibSVMSaver;
  
 public class MainRunner {
 	
@@ -27,54 +36,257 @@ public class MainRunner {
   
 	public static void main(String[] args) throws Exception {
 		
+		// Borrowed some code from: http://www.programcreek.com/2013/01/a-simple-machine-learning-example-in-java/
+		
+		// TODO: Add Cross Validation!
+		
+		// Build the student data
 		BufferedReader testfile = readDataFile("data/students_test.arff");
 		Instances test = new Instances(testfile);
 		test.setClassIndex(1);
 
-		List<Instances> dataList = new ArrayList<Instances>();
+		List<Instances> studentData = new ArrayList<Instances>();
 		
 		for (int i = 10; i <= 100; i += 10) {
 			BufferedReader students = readDataFile("data/Students_" + i + ".arff");
 			Instances trainingData = new Instances(students);
 			trainingData.setClassIndex(1);
-			dataList.add(trainingData);
+			studentData.add(trainingData);
 		}	
 		
-		System.out.println("Training");
-		 
-		for (Instances instances : dataList) {
+		// TODO: Build the other data set
 		
-			// Build the classifier with this instance
-			Classifier cls = new J48();
-			cls.buildClassifier(instances);
-			
-			// Evaluate the classifier with the test data
-			Evaluation eval = new Evaluation(instances);
-			
-			eval.evaluateModel(cls, instances);
-			
-			// Print the result
-			System.out.println(eval.toSummaryString("\nTraining Results\n======\n", false));		
-			
-		}
+		// Call each of the five algorithms		
+		//DecisionTree(studentData, test);
+		//NeuralNetwork(studentData, test);
+		//Boosting(studentData, test);
+		SVM(studentData, test);
+		//KNN(studentData, test);
 		
-		System.out.println("Test");
-		
-		for (Instances instances : dataList) {
-			
-			// Build the classifier with this instance
-			Classifier cls = new J48();
-			cls.buildClassifier(instances);
-			
-			// Evaluate the classifier with the test data
-			Evaluation eval = new Evaluation(instances);
-			
-			eval.evaluateModel(cls, test);
-			
-			// Print the result
-			System.out.println(eval.toSummaryString("\nTest Results\n======\n", false));
-		}
-	
- 
 	}
+	
+	public static void DecisionTree(List<Instances> dataList, Instances testData) throws Exception {
+		
+		System.out.println("STARTING Decision Tree\n");
+		
+		System.out.println("Training Results");
+					
+		for (Instances instances : dataList) {
+			
+			// Build the classifier with this instance
+			Classifier j48 = new J48();			
+			Classifier cls = j48;
+			cls.buildClassifier(instances);
+			
+			// Evaluate the classifier with the test data
+			Evaluation eval = new Evaluation(instances);
+			eval.evaluateModel(cls, instances);			
+			
+			// Print the result
+			System.out.println("Error Rate: " + eval.errorRate() * 100 + "%");
+		}
+		
+		System.out.println("Test Results");
+				
+		for (Instances instances : dataList) {
+		
+			// Build the classifier with this instance
+			Classifier j48 = new J48();		
+			Classifier cls = j48;
+			cls.buildClassifier(instances);
+			
+			// Evaluate the classifier with the test data
+			Evaluation eval = new Evaluation(instances);
+			
+			eval.evaluateModel(cls, testData);
+			
+			// Print the result
+			System.out.println("Error Rate: " + eval.errorRate() * 100 + "%");		
+		}
+		
+	}
+	
+	public static void Boosting(List<Instances> dataList, Instances testData) throws Exception {
+		
+		System.out.println("STARTING Boosting Tree\n");
+		
+		System.out.println("Training Results");
+					
+		for (Instances instances : dataList) {
+			
+			// Build the classifier with this instance
+			Classifier cls = new AdaBoostM1();
+			((AdaBoostM1)cls).setClassifier(new J48());
+			cls.buildClassifier(instances);
+			
+			// Evaluate the classifier with the test data
+			Evaluation eval = new Evaluation(instances);
+			eval.evaluateModel(cls, instances);			
+			
+			// Print the result
+			System.out.println("Error Rate: " + eval.errorRate() * 100 + "%");
+		}
+		
+		System.out.println("Test Results");
+				
+		for (Instances instances : dataList) {
+		
+			// Build the classifier with this instance
+			Classifier cls = new AdaBoostM1();
+			((AdaBoostM1)cls).setClassifier(new J48());			
+			cls.buildClassifier(instances);
+			
+			// Evaluate the classifier with the test data
+			Evaluation eval = new Evaluation(instances);
+			
+			eval.evaluateModel(cls, testData);
+			
+			// Print the result
+			System.out.println("Error Rate: " + eval.errorRate() * 100 + "%");		
+		}		
+	}		
+	
+	public static void NeuralNetwork(List<Instances> dataList, Instances testData) throws Exception {
+		
+		System.out.println("STARTING Neural Network\n");
+		
+		System.out.println("Training Results");
+					
+		for (Instances instances : dataList) {
+			
+			// Build the classifier with this instance
+			MultilayerPerceptron p = new MultilayerPerceptron();			
+			Classifier cls = p;
+			cls.buildClassifier(instances);
+			
+			// Evaluate the classifier with the test data
+			Evaluation eval = new Evaluation(instances);
+			eval.evaluateModel(cls, instances);			
+			
+			// Print the result
+			System.out.println("Error Rate: " + eval.errorRate() * 100 + "%");
+		}
+		
+		System.out.println("Test Results");
+				
+		for (Instances instances : dataList) {
+		
+			// Build the classifier with this instance
+			MultilayerPerceptron p = new MultilayerPerceptron();			
+			Classifier cls = p;	
+			cls.buildClassifier(instances);
+			
+			// Evaluate the classifier with the test data
+			Evaluation eval = new Evaluation(instances);
+			
+			eval.evaluateModel(cls, testData);
+			
+			// Print the result
+			System.out.println("Error Rate: " + eval.errorRate() * 100 + "%");		
+		}
+	}
+	
+	public static void SVM(List<Instances> dataList, Instances testData) throws Exception {
+		
+		// TODO: Use at least two Kernel Functions
+
+		// Some code borrow from: https://stackoverflow.com/questions/5223982/how-to-use-libsvm-with-weka-in-my-java-code
+		
+		System.out.println("STARTING SVM\n");
+		
+		System.out.println("Training Results");
+					
+		for (Instances instances : dataList) {
+			
+			// Build the classifier with this instance
+			
+			LibSVM svm = new LibSVM();
+			Classifier cls = svm;
+			cls.buildClassifier(instances);
+			
+			// Evaluate the classifier with the test data
+			Evaluation eval = new Evaluation(instances);
+			eval.evaluateModel(cls, instances);			
+			
+			// Print the result
+			System.out.println("Error Rate: " + eval.errorRate() * 100 + "%");
+		}
+		
+		System.out.println("Test Results");
+		
+		for (Instances instances : dataList) {
+		
+			// Build the classifier with this instance
+			LibSVM svm = new LibSVM();
+			Classifier cls = svm;
+			cls.buildClassifier(instances);
+			
+			// Evaluate the classifier with the test data
+			Evaluation eval = new Evaluation(instances);
+			
+			eval.evaluateModel(cls, testData);
+			
+			// Print the result
+			System.out.println("Error Rate: " + eval.errorRate() * 100 + "%");		
+		}
+				
+	}
+	
+	public static void KNN(List<Instances> dataList, Instances testData) throws Exception {
+		System.out.println("STARTING KNN\n");
+		
+		List<Integer> knn = Arrays.asList(1, 2, 3, 4, 5, 10, 20);
+		
+		System.out.println("Training Results");
+		for (Integer i : knn) {
+
+			System.out.println("Using KNN: " + i);
+			
+			for (Instances instances : dataList) {
+				
+				// Build the classifier with this instance
+				IBk ibk = new IBk();
+				ibk.setKNN(i);
+				
+				Classifier cls = ibk;
+				cls.buildClassifier(instances);
+				
+				// Evaluate the classifier with the test data
+				Evaluation eval = new Evaluation(instances);
+				eval.evaluateModel(cls, instances);			
+				
+				// Print the result
+				System.out.println("Error Rate: " + eval.errorRate() * 100 + "%");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("Test Results");
+		
+		for (Integer i : knn) {
+		
+			System.out.println("Using KNN: " + i);
+				
+			for (Instances instances : dataList) {
+			
+				// Build the classifier with this instance
+				IBk ibk = new IBk();
+				ibk.setKNN(i);
+			
+				Classifier cls = ibk;
+				cls.buildClassifier(instances);
+				
+				// Evaluate the classifier with the test data
+				Evaluation eval = new Evaluation(instances);
+				
+				eval.evaluateModel(cls, testData);
+				
+				// Print the result
+				System.out.println("Error Rate: " + eval.errorRate() * 100 + "%");		
+			}
+			System.out.println();
+		}
+	}
+	
+	
 }

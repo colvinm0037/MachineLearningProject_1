8756590c0e38d327f2d2cd6165c03564c4b2fa8d
Micah <colvinm0037@my.uwstout.edu>
2017-02-02 09:28:20 -0600

    Refactoring down to one Learner method. Much cleaner.

--- src/com/micah/ml/MainRunner.java
+++ src/com/micah/ml/MainRunner.java
@@ -17,11 +17,14 @@ import weka.classifiers.lazy.IBk;
 import weka.classifiers.meta.AdaBoostM1;
 import weka.classifiers.trees.J48;
 import weka.core.Instances;
-import weka.core.WekaPackageManager;
-import weka.core.converters.LibSVMLoader;
-import weka.core.converters.LibSVMSaver;
  
 public class MainRunner {
+	
+	public static String DECISION_TREE = "DecisionTree";
+	public static String BOOSTING = "Boosting";
+	public static String NEURAL_NETWORK = "NeuralNetwork";
+	public static String SVM = "SVM";
+	public static String KNN = "KNN";
 	
 	public static BufferedReader readDataFile(String filename) {
 		BufferedReader inputReader = null;
@@ -58,211 +61,64 @@ public class MainRunner {
 		}	
 		
 		// TODO: Build the other data set
-		
+		// For the Bike data
+		// MP5 should work
+		// Ibk works
+		// Multilayer Perceptron works
+		// Boosting?
+		// SVM?
+				
 		// Call each of the five algorithms		
-		DecisionTree(studentData, test);
-		Boosting(studentData, test);
-		SVM(studentData, test);
-		KNN(studentData, test);
-		NeuralNetwork(studentData, test);
-	
+		Learn(studentData, test, DECISION_TREE); // Uses J48 Tree
+		Learn(studentData, test, BOOSTING);  // Uses J48 Tree with AdaM1 Boosting
+		Learn(studentData, test, NEURAL_NETWORK); // Uses Multilayer Perceptron
+		Learn(studentData, test, SVM, Arrays.asList(0, 1)); // Uses LibSVM with Linear and Polynomial Kernels
+		Learn(studentData, test, KNN, Arrays.asList(1, 2, 3, 4, 5, 10, 20)); // Uses IBk with K values of 1, 2, 3, 4, 5, 10, 20
+		
 		System.out.println("Program finished.");
 	}
 	
-	public static void DecisionTree(List<Instances> dataList, Instances testData) throws Exception {
-		
-		System.out.println("\nStarting Decision Tree");
-							
-		List<String> trainingRates = new ArrayList<String>();
-		List<String> testingRates = new ArrayList<String>();
-		PrintWriter writer = new PrintWriter("decision_tree.csv", "UTF-8");
-		
-		for (Instances instances : dataList) {
-			
-			// Build the classifier with this instance
-			Classifier j48 = new J48();			
-			Classifier cls = j48;
-			cls.buildClassifier(instances);
-			
-			// Evaluate the classifier with the test data
-			Evaluation eval = new Evaluation(instances);
-			eval.evaluateModel(cls, instances);			
-			
-			trainingRates.add(eval.errorRate() * 100 + "%");
-		}
-						
-		for (Instances instances : dataList) {
-		
-			// Build the classifier with this instance
-			Classifier j48 = new J48();		
-			Classifier cls = j48;
-			cls.buildClassifier(instances);
-			
-			// Evaluate the classifier with the test data
-			Evaluation eval = new Evaluation(instances);
-			
-			eval.evaluateModel(cls, testData);
-			
-			// Print the result
-			testingRates.add(eval.errorRate() * 100 + "%");		
-		}
-		
-		writer.println("Data Used, Training Error, Test Error");
-		for (int i = 0; i < 10; i++) {
-			writer.println(((i + 1) * 10) + "," + trainingRates.get(i) + "," + testingRates.get(i));
-		}
-		writer.close();
-		System.out.println("Decision tree results saved to decision_tree.csv");
+	public static void Learn(List<Instances> dataList, Instances testData, String type) throws Exception {
+		Learn(dataList, testData, type, Arrays.asList(0));
 	}
 	
-	public static void Boosting(List<Instances> dataList, Instances testData) throws Exception {
+	public static void Learn(List<Instances> dataList, Instances testData, String type, List<Integer> versions) throws Exception {
 		
-		System.out.println("\nStarting Boosting Tree");
+		System.out.println("\nStarting " + type);
+		String fileName = type + ".csv";					
 		
-		List<String> trainingRates = new ArrayList<String>();
-		List<String> testingRates = new ArrayList<String>();
-		PrintWriter writer = new PrintWriter("boosting.csv", "UTF-8");
-					
-		for (Instances instances : dataList) {
-			
-			// Build the classifier with this instance
-			Classifier cls = new AdaBoostM1();
-			((AdaBoostM1)cls).setClassifier(new J48());
-			cls.buildClassifier(instances);
-			
-			// Evaluate the classifier with the test data
-			Evaluation eval = new Evaluation(instances);
-			eval.evaluateModel(cls, instances);			
-			
-			// Print the result
-			trainingRates.add(eval.errorRate() * 100 + "%");	
-		}
-						
-		for (Instances instances : dataList) {
-		
-			// Build the classifier with this instance
-			Classifier cls = new AdaBoostM1();
-			((AdaBoostM1)cls).setClassifier(new J48());			
-			cls.buildClassifier(instances);
-			
-			// Evaluate the classifier with the test data
-			Evaluation eval = new Evaluation(instances);
-			
-			eval.evaluateModel(cls, testData);
-			
-			// Print the result
-			testingRates.add(eval.errorRate() * 100 + "%");		
-		}		
-		
-		writer.println("Data Used, Training Error, Test Error");
-		for (int i = 0; i < 10; i++) {
-			writer.println(((i + 1) * 10) + "," + trainingRates.get(i) + "," + testingRates.get(i));
-		}
-		writer.close();
-		System.out.println("Boosting results saved to boosting.csv");
-	}		
-	
-	public static void NeuralNetwork(List<Instances> dataList, Instances testData) throws Exception {
-		
-		System.out.println("\nStarting Neural Network (This usually takes a minute or two to finish)");
-		
-		List<String> trainingRates = new ArrayList<String>();
-		List<String> testingRates = new ArrayList<String>();
-		PrintWriter writer = new PrintWriter("neural_networks.csv", "UTF-8");
-					
-		for (Instances instances : dataList) {
-			
-			// Build the classifier with this instance
-			MultilayerPerceptron p = new MultilayerPerceptron();			
-			Classifier cls = p;
-			cls.buildClassifier(instances);
-			
-			// Evaluate the classifier with the test data
-			Evaluation eval = new Evaluation(instances);
-			eval.evaluateModel(cls, instances);			
-			
-			// Print the result
-			trainingRates.add(eval.errorRate() * 100 + "%");
-		}
-		
-		System.out.println("Test Results");
-				
-		for (Instances instances : dataList) {
-		
-			// Build the classifier with this instance
-			MultilayerPerceptron p = new MultilayerPerceptron();			
-			Classifier cls = p;	
-			cls.buildClassifier(instances);
-			
-			// Evaluate the classifier with the test data
-			Evaluation eval = new Evaluation(instances);
-			
-			eval.evaluateModel(cls, testData);
-			
-			// Print the result
-			testingRates.add(eval.errorRate() * 100 + "%");
-		}
-		
-		writer.println("Data Used, Training Error, Test Error");
-		for (int i = 0; i < 10; i++) {
-			writer.println(((i + 1) * 10) + "," + trainingRates.get(i) + "," + testingRates.get(i));
-		}
-		writer.close();
-		System.out.println("Neural Network results saved to neural_networks.csv");
-	}
-	
-	public static void SVM(List<Instances> dataList, Instances testData) throws Exception {
-		
-		// Some code borrow from: https://stackoverflow.com/questions/5223982/how-to-use-libsvm-with-weka-in-my-java-code
-		
-		System.out.println("\nStarting SVM");
-		
-		PrintWriter writer = new PrintWriter("svm.csv", "UTF-8");
+		PrintWriter writer = new PrintWriter(fileName, "UTF-8");
 		
 		List<List<String>> trainingSets = new ArrayList<List<String>>();
 		List<List<String>> testingSets = new ArrayList<List<String>>();
 		
-		List<Integer> kernels = Arrays.asList(0, 1);
-		String[] kernelNames = new String[]{"Linear", "Polynomial", "Radial Basis Function", "Sigmoid"};
-		
-		for (Integer i : kernels) {
+		for (Integer i : versions) {
 
 			List<String> trainingRates = new ArrayList<String>();
-						
+		
 			for (Instances instances : dataList) {
 				
 				// Build the classifier with this instance
-				LibSVM svm = new LibSVM();
-				String options = ( "-S " + i );
-				String[] optionsArray = options.split( " " );
-				
-				Classifier cls = svm;
-				((AbstractClassifier) cls).setOptions( optionsArray );
+				Classifier cls = buildClassifier(type, i);
 				cls.buildClassifier(instances);
 				
 				// Evaluate the classifier with the test data
 				Evaluation eval = new Evaluation(instances);
 				eval.evaluateModel(cls, instances);			
 				
-				// Print the result
 				trainingRates.add(eval.errorRate() * 100 + "%");
 			}
 			trainingSets.add(trainingRates);
 		}
 		
-		for (Integer i : kernels) {
+		for (Integer i : versions) {
 			
 			List<String> testingRates = new ArrayList<String>();
-			
+									
 			for (Instances instances : dataList) {
 			
 				// Build the classifier with this instance
-				LibSVM svm = new LibSVM();
-				String options = ( "-S " + i );
-				String[] optionsArray = options.split( " " );
-				
-				Classifier cls = svm;
-				((AbstractClassifier) cls).setOptions( optionsArray );
+				Classifier cls = buildClassifier(type, i);
 				cls.buildClassifier(instances);
 				
 				// Evaluate the classifier with the test data
@@ -281,7 +137,7 @@ public class MainRunner {
 			List<String> trainingRates = trainingSets.get(i);
 			List<String> testingRates = testingSets.get(i);
 
-			writer.println("Using Kernel: " + kernelNames[i]);
+			writer.println("Using Option: " + i);
 			writer.println("Data Used, Training Error, Test Error");
 			for (int j = 0; j < 10; j++) {
 				writer.println(((j + 1) * 10) + "," + trainingRates.get(j) + "," + testingRates.get(j));
@@ -290,79 +146,36 @@ public class MainRunner {
 		}
 		
 		writer.close();
-		System.out.println("SVM results saved to svm.csv");
+		System.out.println("Results saved to " + fileName);
 	}
 	
-	public static void KNN(List<Instances> dataList, Instances testData) throws Exception {
+	// Build the classifier for the type passed in. Optionally also pass in an option parameter.
+	public static Classifier buildClassifier(String type, int i) throws Exception {
 		
-		System.out.println("\nStarting KNN");
-		PrintWriter writer = new PrintWriter("knn.csv", "UTF-8");
+		Classifier cls = null;
 		
-		List<List<String>> trainingSets = new ArrayList<List<String>>();
-		List<List<String>> testingSets = new ArrayList<List<String>>();
-		List<Integer> knn = Arrays.asList(1, 2, 3, 4, 5, 10, 20);
-		
-		for (Integer i : knn) {
-			
-			List<String> trainingRates = new ArrayList<String>();
-			
-			for (Instances instances : dataList) {
-				
-				// Build the classifier with this instance
-				IBk ibk = new IBk();
-				ibk.setKNN(i);
-				
-				Classifier cls = ibk;
-				cls.buildClassifier(instances);
-				
-				// Evaluate the classifier with the test data
-				Evaluation eval = new Evaluation(instances);
-				eval.evaluateModel(cls, instances);			
-				
-				// Print the result
-				trainingRates.add(eval.errorRate() * 100 + "%");
-			}
-			trainingSets.add(trainingRates);
+		if (type.equals(DECISION_TREE)) {
+			J48 j48 = new J48();
+			cls = j48;
+		} else if (type.equals(BOOSTING)) {
+			cls = new AdaBoostM1();
+			((AdaBoostM1)cls).setClassifier(new J48());			
+		} else if (type.equals(NEURAL_NETWORK)) {
+			MultilayerPerceptron p = new MultilayerPerceptron();			
+			cls = p;
+		} else if (type.equals(SVM)) {
+			LibSVM svm = new LibSVM();
+			String options = ( "-S " + i );
+			String[] optionsArray = options.split( " " );			
+			cls = svm;
+			((AbstractClassifier) cls).setOptions( optionsArray );
+		} else if (type.equals(KNN)) {
+			IBk ibk = new IBk();
+			ibk.setKNN(i);		
+			cls = ibk;
 		}
 		
-		for (Integer i : knn) {
-		
-			List<String> testingRates = new ArrayList<String>();	
-			
-			for (Instances instances : dataList) {
-			
-				// Build the classifier with this instance
-				IBk ibk = new IBk();
-				ibk.setKNN(i);
-			
-				Classifier cls = ibk;
-				cls.buildClassifier(instances);
-				
-				// Evaluate the classifier with the test data
-				Evaluation eval = new Evaluation(instances);
-				eval.evaluateModel(cls, testData);
-				
-				// Print the result
-				testingRates.add(eval.errorRate() * 100 + "%");		
-			}
-			testingSets.add(testingRates);
-		}
-		
-		for (int i = 0; i < trainingSets.size(); i++) {
-			
-			List<String> trainingRates = trainingSets.get(i);
-			List<String> testingRates = testingSets.get(i);
-
-			writer.println("Using KNN: " + i);
-			writer.println("Data Used, Training Error, Test Error");
-			for (int j = 0; j < 10; j++) {
-				writer.println(((j + 1) * 10) + "," + trainingRates.get(j) + "," + testingRates.get(j));
-			}
-			writer.println();
-		}
-		
-		writer.close();
-		System.out.println("KNN results saved to knn.csv");
+		return cls;
 	}
-	
+		
 }

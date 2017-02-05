e17e17ca64b80e094c517f8604e45d695a1db29e
Micah <colvinm0037@my.uwstout.edu>
2017-02-04 21:34:11 -0600

    Many updates.

--- src/com/micah/ml/MainRunner.java
+++ src/com/micah/ml/MainRunner.java
@@ -7,7 +7,10 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
+import weka.attributeSelection.CfsSubsetEval;
+import weka.attributeSelection.GreedyStepwise;
 import weka.classifiers.AbstractClassifier;
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
@@ -15,7 +18,10 @@ import weka.classifiers.functions.LibSVM;
 import weka.classifiers.functions.MultilayerPerceptron;
 import weka.classifiers.lazy.IBk;
 import weka.classifiers.meta.AdaBoostM1;
+import weka.classifiers.meta.AttributeSelectedClassifier;
 import weka.classifiers.trees.J48;
+import weka.classifiers.trees.M5P;
+import weka.classifiers.trees.REPTree;
 import weka.core.Instances;
  
 public class MainRunner {
@@ -47,9 +53,9 @@ public class MainRunner {
 		System.out.println("Beginning Micah Colvin's Machine Learning Project #1");
 		
 		// Build the student data
-		BufferedReader testfile = readDataFile("data/students_test.arff");
-		Instances test = new Instances(testfile);
-		test.setClassIndex(1);
+		BufferedReader studentTestFile = readDataFile("data/students_test.arff");
+		Instances studentTest = new Instances(studentTestFile);
+		studentTest.setClassIndex(1);
 
 		List<Instances> studentData = new ArrayList<Instances>();
 		
@@ -59,88 +65,98 @@ public class MainRunner {
 			trainingData.setClassIndex(1);
 			studentData.add(trainingData);
 		}	
+
+		// Build the bicycle data
+		BufferedReader bicycleTestFile = readDataFile("data/bikes_cleaned_testing.arff");
+		Instances bikesTest = new Instances(bicycleTestFile);
+		bikesTest.setClassIndex(13);
+
+		List<Instances> bikesData = new ArrayList<Instances>();
 		
-		// TODO: Build the other data set
-		// For the Bike data
-		// MP5 should work
-		// Ibk works
-		// Multilayer Perceptron works
-		// Boosting?
-		// SVM?
-				
-		// Call each of the five algorithms		
-		Learn(studentData, test, DECISION_TREE); // Uses J48 Tree
-		Learn(studentData, test, BOOSTING);  // Uses J48 Tree with AdaM1 Boosting
-		Learn(studentData, test, NEURAL_NETWORK); // Uses Multilayer Perceptron
-		Learn(studentData, test, SVM, Arrays.asList(0, 1)); // Uses LibSVM with Linear and Polynomial Kernels
-		Learn(studentData, test, KNN, Arrays.asList(1, 2, 3, 4, 5, 10, 20)); // Uses IBk with K values of 1, 2, 3, 4, 5, 10, 20
+		for (int i = 10; i <= 100; i += 10) {
+			BufferedReader bikes = readDataFile("data/bikes_cleaned_" + i + ".arff");
+			Instances trainingData = new Instances(bikes);
+			trainingData.setClassIndex(13);
+			bikesData.add(trainingData);
+		}	
+			
+		// TODO: Update methods to allow for multiple data files
+		
+		// Train the bikes data set
+	//	Learn(bikesData, bikesTest, "bikes", DECISION_TREE, Arrays.asList(0, 1, 2));//, 3, 4, 5)); // Uses J48 Tree		
+	//	Learn(bikesData, bikesTest, "bikes", BOOSTING, Arrays.asList(0, 1, 2));  // Uses J48 Tree with AdaM1 Boosting
+	//	Learn(bikesData, bikesTest, "bikes", NEURAL_NETWORK); // Uses Multilayer Perceptron
+	//	Learn(bikesData, bikesTest, "bikes", SVM, Arrays.asList(0, 1, 2, 3)); //, 1, 2, 3)); // Uses LibSVM with Linear and Polynomial Kernels
+		Learn(bikesData, bikesTest, "bikes", KNN, Arrays.asList(1, 2, 3, 4, 5, 10, 20)); // Uses IBk with K values of 1, 2, 3, 4, 5, 10, 20
+		
+		// Train the students data set		
+		// Learn(studentData, studentTest, "students", DECISION_TREE, Arrays.asList(0, 1, 2)); // Uses J48 Tree
+		// Learn(studentData, studentTest, "students", BOOSTING, Arrays.asList(0, 1, 2));  // Uses J48 Tree with AdaM1 Boosting
+		// Learn(studentData, studentTest, "students", NEURAL_NETWORK, Arrays.asList(0, 1)); // Uses Multilayer Perceptron
+		// Learn(studentData, studentTest, "students", SVM, Arrays.asList(0, 1, 2, 3)); // Uses LibSVM with Linear and Polynomial Kernels
+		// Learn(studentData, studentTest, "students", KNN, Arrays.asList(1, 2, 3, 4, 5, 10, 20)); // Uses IBk with K values of 1, 2, 3, 4, 5, 10, 20
 		
 		System.out.println("Program finished.");
 	}
 	
-	public static void Learn(List<Instances> dataList, Instances testData, String type) throws Exception {
-		Learn(dataList, testData, type, Arrays.asList(0));
+	public static void Learn(List<Instances> dataList, Instances testData, String datasetName, String type) throws Exception {
+		Learn(dataList, testData, datasetName, type, Arrays.asList(0));
 	}
 	
-	public static void Learn(List<Instances> dataList, Instances testData, String type, List<Integer> versions) throws Exception {
+	public static void Learn(List<Instances> dataList, Instances testData, String datasetName, String type, List<Integer> versions) throws Exception {
 		
 		System.out.println("\nStarting " + type);
-		String fileName = type + ".csv";					
+		String fileName = type + "_" + datasetName + ".csv";					
 		
 		PrintWriter writer = new PrintWriter(fileName, "UTF-8");
 		
 		List<List<String>> trainingSets = new ArrayList<List<String>>();
 		List<List<String>> testingSets = new ArrayList<List<String>>();
+		List<List<String>> cvSets = new ArrayList<List<String>>();
 		
 		for (Integer i : versions) {
 
 			List<String> trainingRates = new ArrayList<String>();
-		
+			List<String> testingRates = new ArrayList<String>();
+			List<String> cvRates = new ArrayList<String>();
+			
 			for (Instances instances : dataList) {
 				
-				// Build the classifier with this instance
+				// Build the classifier with this instance and train it with the training data
+				Classifier cvCls = buildClassifier(type, i);
+				Evaluation cvEval = new Evaluation(instances);
+				cvEval.crossValidateModel(cvCls, instances, 10, new Random(1));
+				cvRates.add(cvEval.errorRate() * 100 + "%");				
+				
 				Classifier cls = buildClassifier(type, i);
 				cls.buildClassifier(instances);
-				
-				// Evaluate the classifier with the test data
 				Evaluation eval = new Evaluation(instances);
-				eval.evaluateModel(cls, instances);			
 				
+				// Evaluate the classifier with the training data
+				eval.evaluateModel(cls, instances);							
 				trainingRates.add(eval.errorRate() * 100 + "%");
+				
+				// Evaluate the classifier with the testing data
+				Evaluation testEval = new Evaluation(instances);
+				testEval.evaluateModel(cls, testData);							
+				testingRates.add(testEval.errorRate() * 100 + "%");
+				//System.out.println(testEval.toSummaryString());
 			}
 			trainingSets.add(trainingRates);
-		}
-		
-		for (Integer i : versions) {
-			
-			List<String> testingRates = new ArrayList<String>();
-									
-			for (Instances instances : dataList) {
-			
-				// Build the classifier with this instance
-				Classifier cls = buildClassifier(type, i);
-				cls.buildClassifier(instances);
-				
-				// Evaluate the classifier with the test data
-				Evaluation eval = new Evaluation(instances);
-				
-				eval.evaluateModel(cls, testData);
-				
-				// Print the result
-				testingRates.add(eval.errorRate() * 100 + "%");		
-			}
-			testingSets.add(testingRates);			
+			testingSets.add(testingRates);
+			cvSets.add(cvRates);
 		}
 		
 		for (int i = 0; i < trainingSets.size(); i++) {
 			
 			List<String> trainingRates = trainingSets.get(i);
 			List<String> testingRates = testingSets.get(i);
+			List<String> cvRates = cvSets.get(i);
 
 			writer.println("Using Option: " + i);
-			writer.println("Data Used, Training Error, Test Error");
+			writer.println("Data Used, Training Error, Test Error, CV Error");
 			for (int j = 0; j < 10; j++) {
-				writer.println(((j + 1) * 10) + "," + trainingRates.get(j) + "," + testingRates.get(j));
+				writer.println(((j + 1) * 10) + "," + trainingRates.get(j) + "," + testingRates.get(j) + "," + cvRates.get(j));
 			}
 			writer.println();
 		}
@@ -154,22 +170,73 @@ public class MainRunner {
 		
 		Classifier cls = null;
 		
+		System.out.println("I: " + i);
+		
 		if (type.equals(DECISION_TREE)) {
+
 			J48 j48 = new J48();
+			j48.setNumFolds(5);
+			
+			if (i == 1) {				
+				String options = ( "-U");
+				String[] optionsArray = options.split( " " );			
+				cls = j48;
+				((AbstractClassifier) cls).setOptions( optionsArray );	
+				return cls;
+			} else if (i == 2) {
+				j48.setConfidenceFactor(.1f);
+			} else if (i == 3) {
+				j48.setConfidenceFactor(.2f);
+			} else if (i == 4) {
+				j48.setConfidenceFactor(.4f);
+			} else if (i == 5) {
+				j48.setConfidenceFactor(.5f);				
+			}
+			
 			cls = j48;
+			
+			System.out.println("Confidence Factor: " + j48.getConfidenceFactor());
+			
+			
 		} else if (type.equals(BOOSTING)) {
-			cls = new AdaBoostM1();
-			((AdaBoostM1)cls).setClassifier(new J48());			
+			
+			System.out.println("Boosting: I = " + i);
+			
+			AdaBoostM1 ada = new AdaBoostM1();
+			J48 j48 = new J48();			
+			if (i == 1) {
+				j48.setUnpruned(true);
+			} else if (i == 2) {
+				j48.setConfidenceFactor(.5f);
+			}
+
+			cls = ada;
+			((AdaBoostM1)cls).setClassifier(j48);
+
+			// 36 with cf = .5
+			
 		} else if (type.equals(NEURAL_NETWORK)) {
+			
 			MultilayerPerceptron p = new MultilayerPerceptron();			
-			cls = p;
+			
+			if (i == 1) {
+				cls = new AdaBoostM1();
+				((AdaBoostM1)cls).setClassifier(p);
+			} else {
+				cls = p;	
+			}
+			
 		} else if (type.equals(SVM)) {
+			
 			LibSVM svm = new LibSVM();
-			String options = ( "-S " + i );
+			
+			String options = ( "-K " + i + " -S 0 -Z");
 			String[] optionsArray = options.split( " " );			
 			cls = svm;
 			((AbstractClassifier) cls).setOptions( optionsArray );
+			
 		} else if (type.equals(KNN)) {
+			
 			IBk ibk = new IBk();
 			ibk.setKNN(i);		
 			cls = ibk;
